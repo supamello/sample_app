45a122fae69ef15f1469e21f1fa8360a560fb046
Simon Cooke <simonbcooke@gmail.com>
2012-03-27 19:07:13 +0200

    stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,9 @@ module ApplicationHelper
     else
       "#{base_title} | #{@title}"
     end
+    
+    def logo
+      image_tag("/images/logo.png", :alt => "Sample App", :class => "round")
+    end
   end
 end
